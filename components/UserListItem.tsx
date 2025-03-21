42093177632c9fad87b2ac8d32385f7bfe705353
harshsde2 <harshsdeii@gmail.com>
2025-03-21 05:38:18 +0530

    start chatting with diff channal

--- components/UserListItem.tsx
+++ components/UserListItem.tsx
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import React from 'react'
-
+import { useAuth } from '@/providers/AuthProvider'
+import { router } from 'expo-router'
+import { useChatContext } from 'stream-chat-react-native-core'
 const UserListItem = ({ user }: { user: any }) => {
-  return (
-    <View style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: 'gray', backgroundColor: 'white' }}>
-      <Text>{user?.full_name}</Text>
-    </View>
-  )
+
+    const { client } = useChatContext()
+    const { user: me } = useAuth()
+
+    const onPress = async () => {
+        const channel = client.channel('messaging', {
+            members: [me?.id, user?.id]
+        })
+        await channel.watch()
+        router.replace(`/(home)/channel/${channel.cid}` as any)
+    }
+    return (
+        <Pressable onPress={onPress} style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: 'gray', backgroundColor: 'white' }}>
+            <Text>{user?.full_name}</Text>
+        </Pressable>
+    )
 }
 
 export default UserListItem
